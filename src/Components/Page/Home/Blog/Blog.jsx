335d995685fcd5f11fd56edee17db322d93c95b7
Javlon1 <muhamedmijotot@gmail.com>
2023-07-11 09:30:29 +0300

    carusel

--- src/Components/Page/Home/Blog/Blog.jsx
+++ src/Components/Page/Home/Blog/Blog.jsx
@@ -2,70 +2,74 @@ import * as React from 'react'
 import './Blog.scss'
 import { Blogs } from '../../../../Data/Data'
 import Img from '../../../../Assets/Img/img.webp'
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
 import { Link } from 'react-router-dom';
+import { Autoplay, Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default function Blog() {
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-            slidesToSlide: 2
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-            slidesToSlide: 1
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-            slidesToSlide: 1
-        }
-    };
+
     return (
         <section className='blog'>
             <div className='container'>
                 <p className='container__title'>BLOG</p>
                 <h2 className='container__text'>Our Latest Blogs</h2>
-                <Carousel
-                    swipeable={true}
-                    draggable={true}
-                    showDots={true}
-                    responsive={responsive}
-                    ssr={true}
-                    infinite={true}
-                    autoPlay={true}
-                    autoPlaySpeed={2000}
-                    keyBoardControl={true}
-                    customTransition="all 1.5s ease"
-                    transitionDuration={500}
-                    containerClass="carousel-container"
-                    removeArrowOnDeviceType={["tablet", "mobile"]}
-                    dotListClass="custom-dot-list-style"
-                    itemClass="carousel-item-padding-40-px"
+
+                <Swiper
+                    // install Swiper modules
+                    modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    navigation
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 1,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 40,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 50,
+                        },
+                    }}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{ clickable: true }}
+                    scrollbar={{ draggable: true }}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    onSlideChange={() => console.log('slide change')}
                 >
                     {
                         Blogs?.map((e) => (
-                            <Link to='/blog-detail' className='container__a' key={e.id}>
-                                <img className='container__a__img' src={Img} alt="" />
-                                <div className='container__a__item'>
-                                    <h3 className='container__a__item__sbject'>{e.subject}</h3>
-                                    <span className='container__a__item__title'>
-                                        <h2>{e.title}</h2>
+                            <SwiperSlide key={e.id}>
+                                <Link to='/blog-detail' className='container__a'>
+                                    <img className='container__a__img' src={Img} alt="" />
+                                    <div className='container__a__item'>
+                                        <h3 className='container__a__item__sbject'>{e.subject}</h3>
+                                        <span className='container__a__item__title'>
+                                            <h2>{e.title}</h2>
+                                        </span>
+                                        <p className='container__a__item__text'>{e.text}</p>
+                                    </div>
+                                    <span className='container__a__link'>
+                                        <p>Read More</p>
+                                        <i className={`bi ${e.icon}`}></i>
                                     </span>
-                                    <p className='container__a__item__text'>{e.text}</p>
-                                </div>
-                                <span className='container__a__link'>
-                                    <p>Read More</p>
-                                    <i className={`bi ${e.icon}`}></i>
-                                </span>
-                            </Link>
+                                </Link>
+                            </SwiperSlide>
                         ))
                     }
-                </Carousel>
+                </Swiper>
             </div>
         </section>
     )
--- src/Components/Page/Home/Team/Team.jsx
+++ src/Components/Page/Home/Team/Team.jsx
@@ -2,77 +2,81 @@ import * as React from 'react'
 import './Team.scss'
 import { Teams } from '../../../../Data/Data'
 import Img from '../../../../Assets/Img/img.webp'
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
 import { Link } from 'react-router-dom';
+import { Autoplay, Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 
 export default function Team() {
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-            slidesToSlide: 2
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-            slidesToSlide: 1
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-            slidesToSlide: 1
-        }
-    };
     return (
         <section className='team'>
             <div className='container'>
                 <p className='container__title'>TEAM MEMBER</p>
                 <h2 className='container__text'>Meet Our Expert Attorneys</h2>
-                <Carousel
-                    swipeable={true}
-                    draggable={true}
-                    showDots={true}
-                    responsive={responsive}
-                    ssr={true}
-                    infinite={true}
-                    autoPlay={true}
-                    autoPlaySpeed={2000}
-                    keyBoardControl={true}
-                    customTransition="all 1.5s ease"
-                    transitionDuration={500}
-                    containerClass="carousel-container"
-                    removeArrowOnDeviceType={["tablet", "mobile"]}
-                    dotListClass="custom-dot-list-style"
-                    itemClass="carousel-item-padding-40-px"
+
+                <Swiper
+                    modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    navigation
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 1,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 40,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 50,
+                        },
+                    }}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{ clickable: true }}
+                    scrollbar={{ draggable: true }}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    onSlideChange={() => console.log('slide change')}
                 >
                     {
                         Teams?.map((e) => (
-                            <Link to='/team' key={e.id}>
-                                <div className='container__item'>
-                                    <img className='container__item__img' src={Img} alt="" />
-                                    <ul className='container__item__list'>
-                                        {
-                                            e.social?.map((i) => (
-                                                <a href={i.link} target="_blank" className='container__item__list__item' key={i.id}>
-                                                    <i className={`bi bi-${i.icon}`}></i>
-                                                </a>
-                                            ))
-                                        }
-                                    </ul>
-                                    <main className='container__item__main'>
-                                        <h2 className='container__item__main__name'>
-                                            <p>
-                                                {e.fullName}
-                                            </p>
-                                        </h2>
-                                        <p className='container__item__main__position'>{e.position}</p>
-                                    </main>
-                                </div>
-                            </Link>
+                            <SwiperSlide key={e.id}>
+                                <Link to='/team'>
+                                    <div className='container__item'>
+                                        <img className='container__item__img' src={Img} alt="" />
+                                        <ul className='container__item__list'>
+                                            {
+                                                e.social?.map((i) => (
+                                                    <a href={i.link} target="_blank" className='container__item__list__item' key={i.id}>
+                                                        <i className={`bi bi-${i.icon}`}></i>
+                                                    </a>
+                                                ))
+                                            }
+                                        </ul>
+                                        <main className='container__item__main'>
+                                            <h2 className='container__item__main__name'>
+                                                <p>
+                                                    {e.fullName}
+                                                </p>
+                                            </h2>
+                                            <p className='container__item__main__position'>{e.position}</p>
+                                        </main>
+                                    </div>
+                                </Link>
+                            </SwiperSlide>
                         ))
                     }
-                </Carousel>
+                </Swiper>
             </div>
         </section>
     )
