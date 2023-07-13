1db5978819de7d6b7bed84b618da9a82134619de
Javlon1 <muhamedmijotot@gmail.com>
2023-07-13 10:31:20 +0300

    blog

--- src/Components/Page/Blog/Item/Item.jsx
+++ src/Components/Page/Blog/Item/Item.jsx
@@ -1,31 +1,53 @@
 import * as React from 'react'
 import ReactPaginate from 'react-paginate'
 import './Item.scss'
-
-
+import axios from 'axios'
+import Blog from '../function'
 
 export default function Item() {
+    const [blogs, setBlogs] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [currentPage, setCurrentPage] = React.useState(1)
+    const [countriesPerPage] = React.useState(6)
 
-    const [items, setItems] = React.useState([])
+    React.useEffect(() => {
+        const getCounteries = async () => {
+            setLoading(true)
+            const res = await axios.get('https://63c2c490b0c286fbe5f347e9.mockapi.io/users')
+            setBlogs(res.data);
+            setLoading(false)
+
+        }
+        getCounteries()
+    }, [])
+
+    const lastCountryIndex = currentPage * countriesPerPage
+    const firstCountryIndex = lastCountryIndex - countriesPerPage
+    const currentCountry = blogs.slice(firstCountryIndex, lastCountryIndex)
 
     const handlerPageClick = (item) => {
 
-        const correntPage = (item.selected) + 1;
+        const correntPage = (item.selected) + 1
+        setCurrentPage(correntPage)
 
     }
+
+    const totalBlogs = blogs.length
 
     return (
         <section className='item'>
             <div className='container'>
+
+                <Blog blogs={currentCountry} loading={loading}/>
+
                 <ReactPaginate
                     previousLabel={'<<'}
                     nextLabel={'>>'}
                     breakLabel={'...'}
-                    pageCount={25}
+                    pageCount={totalBlogs / countriesPerPage}
                     marginPagesDisplayed={2}
-                    pageRangeDisplayed={2}
                     onPageChange={handlerPageClick}
-                    containerClassName={'pagination justify-content-center'}
+                    containerClassName={'justify-content-center pagination container__list'}
                     pageClassName={'page-item'}
                     pageLinkClassName={'page-link'}
                     previousClassName={'page-item'}
---
+++ src/Components/Page/Blog/function.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { Link } from 'react-router-dom'
+import Loader from '../../Ui/Loader/Loader'
+import Img from '../../../Assets/Img/img.webp'
+import './function.scss'
+
+export default function Blog({ blogs, loading }) {
+
+    if (loading) {
+        return <Loader />
+    } 
+
+    return (
+        <section className='blogf'>
+            {
+                blogs?.map((e)=>(
+                    <Link key={e.id} to='/blog-detail' className='blogf__a'>
+                        <img className='blogf__a__img' src={Img} alt="" />
+                        <div className='blogf__a__item'>
+                            <h3 className='blogf__a__item__sbject'>{e.name}</h3>
+                            <span className='blogf__a__item__title'>
+                                <h2>{e.surname}</h2>
+                            </span>
+                            <p className='blogf__a__item__text'>{e.address}</p>
+                        </div>
+                        <span className='blogf__a__link'>
+                            <p>Read More</p>
+                            <i className={`bi ${e.icon}`}></i>
+                        </span>
+                    </Link>
+                ))
+            }
+        </section>
+    )
+}
